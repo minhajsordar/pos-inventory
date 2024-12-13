010ef74a36f0c874f9d36cd0cb0da46f3db6fb60
Ashra <minhajsorder8205@gmail.com>
2024-12-13 11:38:11 +0600

    msdsaf

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -5,7 +5,8 @@ import axios, { Axios } from 'axios';
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-export const backendUrlLink = 'http://localhost:5003';
+export const backendUrlLink = 'https://pos.begunipik.com';
+// export const backendUrlLink = 'http://localhost:5003';
 export const api = axios.create({
   withCredentials: true,
   baseURL: backendUrlLink,
